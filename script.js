021031507cd904c25d9627923b8efd298734855c
KatynOCtraining <t.theveupolaris@gmail.com>
2024-05-08 00:04:01 +0200

    footer

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+// Fonction de gestion du burger-menu
 function toggleMenu() {
-    var menu = document.querySelector('.burger-menu .menu');
-    menu.classList.toggle('active');
+  var menu = document.querySelector('.burger-menu .menu');
+  menu.classList.toggle('active');
 }
+
+// Fonction pour fermer le menu lorsque l'utilisateur clique en dehors de celui-ci
+document.addEventListener('click', function(event) {
+  var menu = document.querySelector('.burger-menu .menu');
+  var burgerMenu = document.querySelector('.burger-menu');
+  var target = event.target;
+
+  // Vérifier si l'élément cliqué est à l'intérieur du menu burger ou non
+  if (!menu.contains(target) && !burgerMenu.contains(target)) {
+      menu.classList.remove('active'); // Fermer le menu
+  }
+});
